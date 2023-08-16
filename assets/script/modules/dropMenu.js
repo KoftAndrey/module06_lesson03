38eb6504e41d0d5fdef88d5f2ffa4aa483fc1e4a
Andrey <KoftAndrey@gmail.com>
2023-08-16 21:52:08 +0300

    edited after comments

--- assets/script/modules/dropMenu.js
+++ assets/script/modules/dropMenu.js
@@ -1,17 +1,12 @@
 // DOM элементы
-const header = document.querySelector('header');
 const overlay = document.querySelector('.nav-overlay');
-const headerContainer = document.querySelector('.header__container');
 const menuBtn = document.querySelector('.header__menu-button');
-const logo = document.querySelector('.header__logo');
-const navigation = document.querySelector('.header__navigation');
-const headerBtn = document.querySelector('.header__button-wrapper');
+const navigation = document.querySelector('.header__navigation-clone');
+const callBtn = document.querySelector('.header__button-clone');
+const callBtnWrapper = document.querySelector('.header__button-wrapper');
 
 // Анимация
-const easeInOut = time => 0.5 * (1 - Math.cos(Math.PI * time));
-
-// Анимация появления меню
-const dropAnimation = (duration) => {
+const animationFunc = (duration, phase, func) => {
   let startAnimation = NaN;
 
   requestAnimationFrame(function step(timestamp) {
@@ -19,78 +14,78 @@ const dropAnimation = (duration) => {
 
     const progress = (timestamp - startAnimation) / duration;
 
+    let opacityValue;
+    let backgroundValue;
+    if (phase === 'open') {
+      opacityValue = progress;
+      backgroundValue = Math.round(progress * 7) / 10;
+    }
+    if (phase === 'close') {
+      opacityValue = 1 - progress;
+      backgroundValue = 0.7 - (progress * 0.7);
+    }
 
-    navigation.style.opacity = easeInOut(progress);
-    if (window.innerWidth < 601) headerBtn.style.opacity = easeInOut(progress);
-    overlay.style.backgroundColor = `rgba(0,0,0, ${easeInOut(progress) * 0.7})`;
-
-    if (progress < 1) requestAnimationFrame(step);
-  });
-};
-
-// Анимация скрытия меню
-const closeAnimation = (duration, openMenu) => {
-  let startAnimation = NaN;
-
-  requestAnimationFrame(function step(timestamp) {
-    startAnimation ||= timestamp;
-
-    const progress = (timestamp - startAnimation) / duration;
-
-    navigation.style.opacity -= easeInOut(progress);
-    if (window.innerWidth < 601) headerBtn.style.opacity -= easeInOut(progress);
-    overlay.style.backgroundColor = `rgba(0,0,0, ${0.7 - (easeInOut(progress) * 0.7)})`;
+    navigation.style.opacity = opacityValue;
+    callBtnWrapper.style.opacity = opacityValue;
+    overlay.style.backgroundColor = `rgba(0,0,0, ${backgroundValue})`;
 
     if (progress < 1) {
       requestAnimationFrame(step);
     } else {
-      headerContainer.append(navigation);
-      logo.after(navigation);
-      header.append(headerContainer);
-
-      header.classList.remove('header_active');
-      overlay.classList.remove('nav-overlay_visible');
-      headerContainer.classList.remove('header__container_active');
-      navigation.classList.remove('header__navigation_visible');
-      headerBtn.classList.remove('header__button-wrapper_visible');
-
-      menuBtn.addEventListener('click', openMenu);
+      func();
     }
   });
 };
 
+// func при открытии
+const openFunc = () => {
+  navigation.style.opacity = 1;
+  callBtnWrapper.style.opacity = 1;
+};
+
+// func при закрытии
+const closeFunc = () => {
+  navigation.style.opacity = 0;
+  callBtnWrapper.style.opacity = 0;
+
+  overlay.classList.remove('nav-overlay_visible');
+  navigation.classList.remove('header__navigation-clone_visible');
+  callBtnWrapper.classList.remove('header__button-wrapper_visible');
+  callBtn.classList.remove('header__button-clone_visible');
+};
+
 // Открытие меню
 const openMenu = () => {
-  overlay.append(headerContainer);
-
-  header.classList.add('header_active');
-  headerContainer.classList.add('header__container_active');
   menuBtn.classList.add('header__menu-button_close');
-  navigation.classList.add('header__navigation_visible');
-  headerBtn.classList.add('header__button-wrapper_visible');
+  navigation.classList.add('header__navigation-clone_visible');
+  callBtnWrapper.classList.add('header__button-wrapper_visible');
+  callBtn.classList.add('header__button-clone_visible');
   overlay.classList.add('nav-overlay_visible');
 
   menuBtn.removeEventListener('click', openMenu);
 
-  dropAnimation(400);
+  animationFunc(300, 'open', openFunc);
 };
-
 
 // Закрытие меню
 const closeMenu = () => {
   menuBtn.classList.remove('header__menu-button_close');
-  closeAnimation(600, openMenu);
+  animationFunc(300, 'close', closeFunc);
+
+  menuBtn.removeEventListener('click', closeMenu);
+  menuBtn.addEventListener('click', openMenu);
 };
 
 // Добавление отслеживания событий
 const menuControl = () => {
-  menuBtn.addEventListener('click', openMenu);
+  menuBtn.addEventListener('click', () => {
+    openMenu();
+    menuBtn.addEventListener('click', closeMenu);
+  });
 
   overlay.addEventListener('click', e => {
-    const target = e.target;
-    if (target === overlay ||
-        target.classList.contains('header__menu-button') ||
-        target.classList.contains('header__menu-button-line')) {
+    if (e.target === overlay ||
+        e.target.classList.contains('header__navigation-clone-link')) {
       closeMenu();
     }
   });
